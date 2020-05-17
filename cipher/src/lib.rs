89e10371e5476123262a878c38afabe6fdfd4031
Mike Dula <mikedula368@gmail.com>
2020-05-17 18:28:16 +0800

    overthinking on how to handle checking of args

--- cipher/src/lib.rs
+++ cipher/src/lib.rs
@@ -1,20 +1,27 @@
-use std::{env,process};
+use std::env;
 
 pub fn initialize(){
-    get_args();
+    let result = get_args();
+    println!("yey");
 }
 
-fn get_args() {
+// @ Mike Dula
+// @ 2020-5-17 18:19
+// @ function to get args passed by the user.
+// @ should return a String if an error occurs,
+// @ and a Vec<String> for the args if successful
+fn get_args() -> Result<Vec<String>, String> {
     let args: Vec<String> = env::args().collect();
     if args.len() < 3 {
-        println!(
+        let err_message = 
             "more args needed.\n\
             1st would be path of message to encrypt\n\
             2nd would be the encryption key\n\
             example: `cipher message.txt secret_key`\n\
             or run: `cipher --help` for more info\n\
-            terminating program… kthxbye…"
-                    );
-        process::exit(0);
+            terminating program… kthxbye…";
+        Err(err_message.to_string())
+    } else {
+        Ok(args)
     }
 }
