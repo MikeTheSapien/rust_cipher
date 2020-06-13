18302d3b80cb91d28606b58e14479fff68e26d87
Mike Dula <mikedula368@gmail.com>
2020-06-13 20:37:50 +0800

    successfully converted char type to an integer

--- cipher/src/lib.rs
+++ cipher/src/lib.rs
@@ -8,9 +8,13 @@ use models::user_input::UserInput;
 pub fn encrypt(ui: UserInput) -> Result<(), Box<dyn Error>> {
     println!("printing with key: {}", ui.key);
     let message = fs::read_to_string(ui.message_path)?;
-    
-    for c in message.bytes() {
-        println!("{}",c + ui.key);
+    // for c in message.bytes() {
+    for c in message.chars() {
+        // const RADIX: u32 = 10;
+        // println!("{:?}",c.to_digit(RADIX));
+        // let new = u32::from_be_bytes(c);
+        println!("{}", c as u32);
     }
+    // println!("{:#?}",message.chars());
     Ok(())
 }
