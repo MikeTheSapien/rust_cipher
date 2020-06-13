656d30fd37785bc45c73e6437a1fd1fd9d59613a
Mike Dula <mikedula368@gmail.com>
2020-06-13 16:57:18 +0800

    fixed what UserInput::new() returns if user had inputted invalid arguments

--- cipher/src/models/user_input.rs
+++ cipher/src/models/user_input.rs
@@ -5,20 +5,20 @@ pub struct UserInput {
 
 impl UserInput {
     pub fn new(args: Vec<String>) -> Result<UserInput, &'static str> {
-    /*
-    @ Mike Dula
-    @ 2020-5-27 20:30
-    @ constructor for UnserInput struct
-    ===params===
-    args: Vec<String> ==> the arguments passed by the user,
-                            where 1st would be for the path of the file to be  
-                            encrypted.
-                            2nd would be the key to encrypt the file.
+        /*
+        @ Mike Dula
+        @ 2020-5-27 20:30
+        @ constructor for UnserInput struct
+        ===params===
+        args: Vec<String> ==> the arguments passed by the user,
+                                where 1st would be for the path of the file to be
+                                encrypted.
+                                2nd would be the key to encrypt the file.
 
-    ===returns===
-    UserInput ==> if parsing of arguments is successful
-    &str ==> if parsing failed, an error message is returned
-    */
+        ===returns===
+        UserInput ==> if parsing of arguments is successful
+        Err(&str) ==> if parsing failed, an error message is returned
+        */
         if args.len() < 3 {
             let err_message = "more args needed. \
              1st would be path of message to encrypt. \
@@ -26,22 +26,21 @@ impl UserInput {
              example: `cipher message.txt secret_key`\n\
              or run: `cipher --help` for more info\n\
              terminating program… kthxbye…";
-            Err(err_message)
-        } else {
-            let key = args[2].parse::<u32>().unwrap_or_else(|err| {
-                println!("key must be a positive integer. {}", err);
-                0
-            });
-            // let key = match args[2].parse::<u32>() {
-            //     Ok(n) => n,
-            //     Err(e) => e,
-            // };
-
-            let config = UserInput {
-                message_path: args[1].clone(),
-                key,
-            };
-            Ok(config)
+            return Err(err_message);
         }
+        let key = args[2].parse::<u32>().unwrap_or_else(|_err| {
+            0
+        });
+
+        if key == 0 {
+            let err_message = "key must be a positive integer.";
+            return Err(err_message);
+        }
+
+        let config = UserInput {
+            message_path: args[1].clone(),
+            key,
+        };
+        Ok(config)
     }
 }
